0bad1e5217939b73ec3dbc5547fcdfe2409f73f3
Matt Gauger <matt.gauger@gmail.com>
2014-03-20 13:53:45 -0500

    Working RPC to serverside

--- src/castra/gleam/api/gleam.clj
+++ src/castra/gleam/api/gleam.clj
@@ -1,1 +1,12 @@
-(ns gleam.api.gleam)
+(ns gleam.api.gleam
+  (:refer-clojure :exclude [defn])
+  (:require [tailrecursion.castra :refer [defn ex error *session*]]))
+
+(def articles {:total 422
+               :ingested 419
+               :fetched 0
+               :errored 0
+               :read 315})
+
+(defn get-state []
+  {:articles articles})
--- src/cljs/gleam/rpc.cljs
+++ src/cljs/gleam/rpc.cljs
@@ -1,16 +1,23 @@
 (ns gleam.rpc
   (:require-macros
     [tailrecursion.javelin :refer [defc defc= cell=]])
-  (:require
-    [clojure.set           :as cs]
-    [clojure.string        :as s]
-    [tailrecursion.javelin :as j :refer [cell]]
-    [tailrecursion.castra  :as c :refer [mkremote]]))
+  (:require [clojure.set           :as cs]
+            [clojure.string        :as s]
+            [tailrecursion.javelin :as j :refer [cell]]
+            [tailrecursion.castra  :as c :refer [mkremote]]))
 
 (set! cljs.core/*print-fn* #(.log js/console %))
 
-(def articles {:total 422
-               :ingested 419
-               :fetched 0
-               :errored 0
-               :read 315})
+(defc state {:articles {}})
+(defc error nil)
+(defc loading [])
+
+(defc= articles (get state :articles))
+
+(def get-state
+  (mkremote 'gleam.api.gleam/get-state state error loading))
+
+(defn init []
+  (get-state)
+  ;; Check every second for new data
+  (js/setInterval get-state 1000))
